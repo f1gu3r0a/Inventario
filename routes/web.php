deca8907dbe9ba2c6b95b223c7c28cd6d0b45faa
fernando <figuafernando@gmail.com>
2024-07-27 16:54:18 -0600

    nuevo

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
