04af6bae4335a5a13f6b081a0e8832a88c7b5878
fernando <figuafernando@gmail.com>
2024-07-27 16:53:43 -0600

    actualiacion

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,6 +10,5 @@ Route::get('/', function () {
 
 // Rutas para el CRUD de productos
 Route::resource('products', ProductController::class);
-use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
